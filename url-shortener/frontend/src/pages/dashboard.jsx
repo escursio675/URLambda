033a7ff130f4abbbc737e0124c090063dff6ac69
CodeCrafter <roktimbailung100@gmail.com>
2026-05-11 22:36:38 +0530

    code restrucute and minor formating changes

--- url-shortener/frontend/src/pages/dashboard.jsx
+++ url-shortener/frontend/src/pages/dashboard.jsx
@@ -93,50 +93,21 @@ export default function Dashboard(){
                     mouseInfluence={0.25}
                 />
             </div>
-            <div className="
-                min-h-screen
-                bg-[#0a0a0a]
-                text-white
-                flex
-                items-center
-                justify-center
-                px-6
-                py-12
-            ">
+            <div className="min-h-screen bg-[#0a0a0a] text-white flex items-center justify-center px-6 py-12">
 
-                <div className="
-                    w-full
-                    max-w-3xl
-                    bg-black/15
-                    border border-white/10
-                    backdrop-blur-2xl
-                    rounded-2xl
-                    p-10
-                    shadow-xl
-                ">
+                <div className=" w-full max-w-3xl bg-black/1 
+                    border border-white/10 backdrop-blur-2xl rounded-2xl p-10 shadow-xl">
 
-                    <h1 className="
-                        text-4xl
-                        font-bold
-                        mb-4
-                        text-center
-                    ">
+                    <h1 className="text-4xl font-bold mb-4 text-center">
                         All Links
                     </h1>
 
-                    <div className="
-                        flex
-                        flex-col
-                        gap-6
-                    ">
+                    <div className="flex flex-col gap-6">
 
                         {
                             urls.length === 0 ? (
 
-                                <p className="
-                                    text-center
-                                    text-purple-400
-                                ">
+                                <p className="text-center text-purple-400">
                                     No URLs created yet
                                 </p>
 
@@ -146,36 +117,17 @@ export default function Dashboard(){
 
                                     <div
                                         key={url._id}
-                                        className="
-                                            bg-black/30
-                                            border border-white/10
-                                            rounded-xl
-                                            p-5
-                                        "
-                                    >
+                                        className="bg-black/30 border border-white/10 rounded-xl p-5">
 
-                                        <p className="
-                                            text-lg
-                                            text-gray-200
-                                            mb-2
-                                        ">
+                                        <p className="text-lg text-gray-200 mb-2">
                                             Long URL
                                         </p>
 
-                                        <p className="
-                                            break-all
-                                            text-xl
-                                            mb-5
-                                            text-xl
-                                        ">
+                                        <p className="break-all text-xl mb-5 text-xl">
                                             {url.longUrl}
                                         </p>
 
-                                        <p className="
-                                            text-lg
-                                            text-gray-200
-                                            mb-2
-                                        ">
+                                        <p className="text-lg text-gray-200 mb-2">
                                             Short URL
                                         </p>
 
@@ -183,37 +135,19 @@ export default function Dashboard(){
                                             href={url.shortUrl}
                                             target="_blank"
                                             rel="noreferrer"
-                                            className="
-                                                text-purple-400
-                                                hover:underline
-                                                break-all
-                                            "
-                                        >
+                                            className="text-purple-400 hover:underline break-all">
                                             {url.shortUrl}
                                         </a>
 
-                                        <div className="
-                                            flex
-                                            items-center
-                                            gap-3
-                                            mt-5
-                                        ">
+                                        <div className="flex items-center gap-3 mt-5">
 
                                             <a
                                                 href={`/?url=${encodeURIComponent(url.shortUrl)}`}
                                                 target="_blank"
                                                 rel="noreferrer"
                                                 className="
-                                                    px-6 
-                                                    py-2 
-                                                    rounded-lg 
-                                                    bg-purple-600 
-                                                    text-white 
-                                                    font-semibold 
-                                                    hover:scale-105 
-                                                    transition
-                                                "
-                                            >
+                                                    px-6 py-2 rounded-lg bg-purple-600 text-white font-semibold 
+                                                    hover:scale-105 transition">
                                                 Open URL Generation Window
                                             </a>
 
@@ -222,9 +156,9 @@ export default function Dashboard(){
                                                     handleDelete(url._id)
                                                 }
                                                 className="
-                                                    px-4
-                                                    py-2
-                                                    border border-gray-300 dark:border-white/20 dark:text-red-400 dark:hover:bg-white/5 rounded-lg text-center text-sm font-semibold text-red-400 transition-colors
+                                                    px-4 py-2 border border-gray-300 dark:border-white/20 
+                                                    dark:text-red-400 dark:hover:bg-white/5 rounded-lg 
+                                                    text-center text-sm font-semibold text-red-400 transition-colors
                                                 "
                                             >
                                                 Delete URL Entry
